4125a3b2f0cc9c1eea1594494e3c226b81195726
Zeke Sikelianos <zeke@sikelianos.com>
2014-10-02 13:39:08 -0700

    use `set -x` in the build script so comments aren’t necessary

--- bin/build.sh
+++ bin/build.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-echo "Copying npm docs to content directory"
+set -x
+
 cp -r node_modules/npm/doc/ content/
-
-echo "Creating a tree of markdown files"
 ./bin/tree.js
-
-echo "Creating an HTML table of contents"
 ./bin/nav.js
