0d5a7d870ca2f62b9187f6853c1ade057b6b55b9
Zeke Sikelianos <zeke@sikelianos.com>
2014-10-03 23:16:15 -0700

    Leave the API docs out for a minute

--- bin/build.sh
+++ bin/build.sh
@@ -7,7 +7,7 @@ rm -rf content/cli
 rm -rf content/files
 rm -rf content/misc
 
-cp -r node_modules/npm/doc/api content/
+# cp -r node_modules/npm/doc/api content/
 cp -r node_modules/npm/doc/cli content/
 cp -r node_modules/npm/doc/files content/
 cp -r node_modules/npm/doc/misc content/
