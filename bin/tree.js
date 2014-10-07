0b4d099841cefb73e7a5473e6789de89cbf5d001
Zeke Sikelianos <zeke@sikelianos.com>
2014-10-06 23:33:20 -0700

    simplify tree generation with hardcoded section content

--- bin/tree.js
+++ bin/tree.js
@@ -7,6 +7,7 @@ var path        = require("path")
 var fs          = require("fs")
 var marked      = require("marked")
 var fmt         = require("util").format
+var strftime    = require("strftime")
 var _           = require("lodash")
 var compact     = _.compact
 var uniq        = _.uniq
@@ -14,13 +15,20 @@ var pluck       = _.pluck
 var merge       = _.merge
 var fm          = require("html-frontmatter")
 var contentFile = path.resolve(__dirname, "../content.json")
-var liteFile = path.resolve(__dirname, "../content.lite.json")
+
+// Flesh out the content object
 var content = {
-  sections: [],
+  sections: [
+    {id: "misc", title: "Using npm"},
+    {id: "enterprise", title: "npm Enterprise"},
+    {id: "cli", title: "CLI Commands"},
+    {id: "files", title: "Configuring npm"},
+    {id: "api", title: "Using npm programmatically"},
+  ],
   pages: []
 }
 
-// Walk the content directory tree
+// Walk around looking for pages
 var emitter = require("walkdir")(path.resolve(__dirname,  "../content/"))
 
 emitter.on("file", function(filename,stat){
@@ -30,9 +38,6 @@ emitter.on("file", function(filename,stat){
 
   var page = {}
   page.content = fs.readFileSync(filename).toString()
-
-  // Get modified date
-  page.modified = fs.statSync(filename).mtime
 
   // Look for HTML frontmatter
   merge(page, fm(page.content))
@@ -45,19 +50,24 @@ emitter.on("file", function(filename,stat){
     page.content = page.content.replace(manHead[0], "")
   }
 
-  // Convert markdown to HTML
+  // Get modified date
+  page.modified = fs.statSync(filename).mtime
+  page.modifiedPretty = strftime("%B %d, %Y", page.modified)
+
+  // Titlecase some things
   page.content = page.content
     .replace(/## SYNOPSIS/g, "## Synopsis")
     .replace(/## DESCRIPTION/g, "## Description")
+    .replace(/## CONFIGURATION/g, "## Configuration")
     .replace(/## SEE ALSO/g, "## See Also")
 
   // Convert markdown to HTML
   page.content = marked(page.content)
 
-  // Clean up the filename
+  // Remove basepath and extension from filename
   filename = filename
-    .replace(/.*\/content\//, "") // remove basepath
-    .replace(/\.md$/, "")  // remove extension
+    .replace(/.*\/content\//, "")
+    .replace(/\.md$/, "")
 
   // Use filename as title if not specified in frontmatter
   // (and remove superfluous npm- prefix)
@@ -73,6 +83,7 @@ emitter.on("file", function(filename,stat){
   } else if (page.section) {
     page.edit_url = "https://github.com/npm/docs.npmjs.com/edit/master/content/" + filename + ".md"
   }
+
   page.href = "/" + filename
 
   content.pages.push(page)
@@ -86,45 +97,8 @@ emitter.on("end",function(){
     return page.title.toLowerCase();
   })
 
-  // var sections = compact(uniq(pluck(content.pages, 'section'))).sort()
-  var sections = ["misc", "enterprise", "cli", "files", "api"]
-
-  content.sections = sections.map(function(id) {
-
-    var section = {
-      id: id,
-      title: id
-        .replace(/^cli$/, "The Command Line Client")
-        .replace(/^api$/, "Using npm Programmatically")
-        .replace(/^files$/, "Configuring npm")
-        .replace(/^misc$/, "Using npm")
-    }
-
-    // Look for a page named "index" in this section
-    var indexPage = _.find(content.pages, function(page) {
-      return path.basename(page.href) === "index" && page.section === section.id
-    })
-
-    // Inherit title and href from index page
-    if (indexPage) {
-      section.title = indexPage.title
-      section.href = indexPage.href
-    }
-
-    return section
-  })
-
   fs.writeFileSync(contentFile, JSON.stringify(content, null, 2))
   console.log("Wrote %s", path.relative(process.cwd(), contentFile))
-
-  var lite = merge({}, content)
-  lite.pages = lite.pages.map(function(page){
-    delete page.content
-    return page
-  })
-
-  fs.writeFileSync(liteFile, JSON.stringify(lite, null, 2))
-  console.log("Wrote %s", path.relative(process.cwd(), liteFile))
 
 
 })
