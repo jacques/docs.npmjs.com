2ff89c3264d71dc946c974f4417a4ba1d6d05266
Zeke Sikelianos <zeke@sikelianos.com>
2014-10-13 15:13:39 -0700

    show suggestions on the 404 page

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ var hbs = require("hbs")
 var harp = require("harp")
 var path = require("path")
 var cors = require("cors")
+var distance = require("leven")
 var find = require("lodash").find
 var merge = require("lodash").merge
+var sortBy = require("lodash").sortBy
 
 // Load section and page data
 var content = require(path.resolve(__dirname, "content.json"))
@@ -45,11 +47,34 @@ app.get("/*", function(req, res) {
   })
 
   if (!page) {
-    return res.status(404).render("404", {
+
+    var ctx = {
       url: req.url,
       pageId: "fourohfour",
       content: content
+    }
+
+    // Find pages with similar HREFs
+    var suggestions = content.pages
+      .filter(function(page) {
+        return distance(req.path, page.href) < 4
+      })
+
+    suggestions = sortBy(suggestions, function(page) {
+      return distance(req.path, page.href)
     })
+      .slice(0,3)
+
+    suggestions.forEach(function(suggestion){
+      console.log(suggestion.href, distance(suggestion.href, req.path))
+    })
+
+    if (suggestions.length) {
+      ctx.suggestions = suggestions
+      ctx.suggestionCount = suggestions.length
+    }
+
+    return res.status(404).render("404", ctx)
   }
 
   res.render("page", {
